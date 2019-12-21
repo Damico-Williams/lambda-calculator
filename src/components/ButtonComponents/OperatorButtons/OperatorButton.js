526eaca557046ef010ac12d21426fb0de982262d
damico williams <damicowilliams@gmail.com>
2019-12-20 18:19:53 -0800

    MVP

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -4,7 +4,7 @@ export const OperatorButton = (props) => {
   return (
     <div className="numButtons2">
     <button>{props.operator}
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {/* Display a button element rendering the data being passed down from the parent container on props*/}
     </button>
     </div>
   );
